984ac2c265adeaef67b52a8784c38390989f9a8d
psyfer <psyferblock.io@gmail.com>
2023-08-23 10:35:36 +0300

    form design ui

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -12,8 +12,22 @@ class RegisterController extends Controller
         return view('auth.register'); // here we are setting the location of the view in advance thinking of the auth directory that has a register file waiting to beused      
     }
 
-    public function store(){
-         dd('congrats you have registered'); // dd (die dump) will effectively kill the page and output anything you have there including variable or string
+    public function store( Request $request ){ // remember $request is the variable object from the input 
+        //  dd('congrats you have registered'); // dd (die dump) will effectively kill the page and output anything you have there including variable or string 
+
+        //VALIDATION 
+        //->validate the user 
+        $this->validate($request,[
+            'name'=>['required','max:255'],//same as the one below just different syntax
+            'username'=>'required|max:255',
+            'email'=> 'required|email|max:255',
+            'password'=>'required|confirmed', //ther confirmed will look for any data submitted with an _confirmed field and make sure the 2 fields match 
+            
+
+        ]);
+        //-> store user
+        //->sign user in
+        //->redirect user to other page 
     }
 }
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,23 +11,58 @@ i imagine there is also one that we can do for a footer  -->
                     <!-- cross site request forgery check to pass the token you can inspectt the source of hte page (/register) to see the token generated when the form is uploaded to the page -->
                     <div class="mb-4"> 
                          <label for="name" class="sr-only">Name</label>
-                         <input type="text" name="name" id="name" placeholder="your Name" class="bg-gray-100 border-2 w-full p-4 rounded-lg"value="">
+                         <input type="text" name="name" id="name" placeholder="your Name" class="bg-gray-100 border-2 w-full p-4 rounded-lg 
+                         @error('name') border-red-600 @enderror 
+                         "value="{{old('name')}}">
+                         @error('name')
+                         <div class=" text-red-500 mt-2 text-sm ">
+                              <!-- scoped message vsriable will be the text outputted   -->
+                              {{$message}} 
+                         </div>
+                         @enderror
                     </div>
                     <div class="mb-4"> 
-                         <label for="userName" class="sr-only">Username</label>
-                         <input type="text" name="userName" id="userName" placeholder="your userName" class="bg-gray-100 border-2 w-full p-4 rounded-lg"value="">
+                         <label for="username" class="sr-only">Username</label>
+                         <!-- the value we insert the old(' ') helper that connects to the name of hte input so it stores the last saved inpur.( state in js) -->
+                         <input type="text" name="username" id="username" placeholder="your Username" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('username') border-red-600 @enderror "
+                         value="{{old('username')}} 
+                         ">
+                         @error('username')
+                         <div class=" text-red-500 mt-2 text-sm ">
+                              <!-- scoped message vsriable will be the text outputted   -->
+                              {{$message}} 
+                         </div>
+                         @enderror
                     </div>
                     <div class="mb-4"> 
                          <label for="email" class="sr-only">Email</label>
-                         <input type="text" name="email" id="email" placeholder="your Email" class="bg-gray-100 border-2 w-full p-4 rounded-lg"value="">
+                         <input type="text" name="email" id="email" placeholder="your Email" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('email') border-red-600 @enderror "value="{{old('email')}}">
+                         @error('email')
+                         <div class=" text-red-500 mt-2 text-sm ">
+                              <!-- scoped message vsriable will be the text outputted   -->
+                              {{$message}} 
+                         </div>
+                         @enderror
                     </div> 
                      <div class="mb-4"> 
                          <label for="password" class="sr-only">Password</label>
-                         <input type="text" name="password" id="password" placeholder="your Password" class="bg-gray-100 border-2 w-full p-4 rounded-lg"value="">
+                         <input type="text" name="password" id="password" placeholder="your Password" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password') border-red-600 @enderror "value="{{old('password')}}">
+                         @error('password')
+                         <div class=" text-red-500 mt-2 text-sm ">
+                              <!-- scoped message vsriable will be the text outputted   -->
+                              {{$message}} 
+                         </div>
+                         @enderror
                     </div>
                     <div class="mb-4"> 
                          <label for="password_confirmation" class="sr-only">repeat password</label>
-                         <input type="text" name="password_confirmation" id="password_confirmation" placeholder="Repeat your Password" class="bg-gray-100 border-2 w-full p-4 rounded-lg"value="">
+                         <input type="text" name="password_confirmation" id="password_confirmation" placeholder="Repeat your Password" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('password_confirmation') border-red-600 @enderror "value="{{old('password_confirmation')}}">
+                         @error('password_confirmation')
+                         <div class=" text-red-500 mt-2 text-sm ">
+                              <!-- scoped message vsriable will be the text outputted   -->
+                              {{$message}} 
+                         </div>
+                         @enderror
                     </div>
                     <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Register</button>
 
